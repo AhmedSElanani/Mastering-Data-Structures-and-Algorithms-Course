6ddff9e48faa02d10f9717954322c3f29fd29eda
Ahmed Salah <AhmedSalahAlanani@gmail.com>
2023-08-20 16:59:43 +0200

    Add CppCheck to the project

---
+++ scripts/run-cppcheck.sh
@@ -0,0 +1,13 @@
+#!/bin/bash
+
+# Specify the directories to analyze
+DIRECTORIES=(
+    'src'
+    'test'
+)
+
+# Run Cppcheck on each directory's source files
+# TODO: parse build configuration for --std=c++20 instead of hardcoded or any similar approach.
+for DIR in "${DIRECTORIES[@]}"; do
+    cppcheck --enable=all --suppressions-list='cppcheck-suppress-list.txt' --std=c++20 $(find "$DIR" -type f \( -regex ".*\.[ch]?.pp" -o -name "*.h" \))
+done
