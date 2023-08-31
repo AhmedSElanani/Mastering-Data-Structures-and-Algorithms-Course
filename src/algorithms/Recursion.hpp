2f11c646407aee3ed62fa3637fa8c974045e8e7a
Ahmed Salah <AhmedSalahAlanani@gmail.com>
2023-08-31 17:26:14 +0200

    Add power method to Recursion class
    - Added a method that recusrively raises the power of a given
      number to the specified input.

--- src/algorithms/Recursion.hpp
+++ src/algorithms/Recursion.hpp
@@ -19,11 +19,29 @@ public:
   static constexpr auto sumNaturalNums(NaturalNumber auto num) -> std::size_t {
     return (num == 0U || num == 1U) ? num : num + sumNaturalNums(num - 1U);
   }
+
   /// @brief method returns the factorial of the natural number N
   /// @param num input N value
   /// @return the factorial of the natural number N
   static constexpr auto factorial(NaturalNumber auto num) -> std::size_t {
     return (num == 0U || num == 1U) ? 1U : num * factorial(num - 1U);
+  }
+
+  /// @brief method returns the number raised to a certain power
+  /// @param num the number to be raised to a power
+  /// @param pow a natural number to be raised to
+  /// @return the number raised to the power
+  template <typename T>
+  static constexpr auto power(T num, NaturalNumber auto pow) -> T {
+    if (pow == 0U) {
+      return 1;
+    }
+
+    if (pow == 1U) {
+      return num;
+    }
+
+    return num * power(num, pow - 1U);
   }
 };
 }  // namespace algorithms
