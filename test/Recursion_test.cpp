68593de39026ccbfc0941e8314198a91ee5d9f68
Ahmed Salah <AhmedSalahAlanani@gmail.com>
2023-08-31 13:35:42 +0200

    Add unit tests for factorial method

--- test/Recursion_test.cpp
+++ test/Recursion_test.cpp
@@ -25,5 +25,26 @@ TEST_F(SumNaturalNumsTest, PositiveInputs) {
 }
 }  // namespace algorithms_test::sumNaturalNums_test
 
+/// @brief class for testing factorial method
+namespace algorithms_test::factorial_test {
 
-}  // namespace algorithms_test
+/// @brief class for testing factorial method
+class FactorialTest : public ::testing::Test {};
+
+TEST_F(FactorialTest, FactorialZero) {
+  EXPECT_EQ(::algorithms::Recursion::factorial(0U), 1U);
+}
+
+TEST_F(FactorialTest, FactorialOne) {
+  EXPECT_EQ(::algorithms::Recursion::factorial(1U), 1U);
+}
+
+TEST_F(FactorialTest, PositiveInputs) {
+  EXPECT_EQ(::algorithms::Recursion::factorial(2U), 2U);
+  EXPECT_EQ(::algorithms::Recursion::factorial(3U), 6U);
+  EXPECT_EQ(::algorithms::Recursion::factorial(4U), 24U);
+  EXPECT_EQ(::algorithms::Recursion::factorial(5U), 120U);
+  EXPECT_EQ(::algorithms::Recursion::factorial(6U), 720U);
+}
+
+}  // namespace algorithms_test::factorial_test
