cc3bff37d4e24d2c8c81b175c0aa53aa21be07b2
Ahmed Salah <AhmedSalahAlanani@gmail.com>
2024-01-17 07:21:11 +0100

    Add skeleton for Array ADT
    - Added a new header for Array ADT
    - Addded one new example test for Array ADT
    - Updated CMake accordingly

---
+++ src/data-structures/ArrayAdt.hpp
@@ -0,0 +1,9 @@
+#pragma once
+
+/// @brief namespace for data structures implemented
+namespace dataStructures {
+
+/// @brief class representing Array abstract data type
+class ArrayAdt {};
+
+}  // namespace dataStructures
---
+++ test/ArrayAdt_test.cpp
@@ -0,0 +1,10 @@
+#include "data-structures/ArrayAdt.hpp"
+
+#include "gtest/gtest.h"
+
+/// @brief namespace for arrayAdt_test tests
+namespace dataStructures_test::arrayAdt_test {
+using ::testing::Test;
+
+TEST(ExampleTestSuite, ExampleTest) { ASSERT_TRUE(true); }
+}  // namespace dataStructures_test::arrayAdt_test
