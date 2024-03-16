36d3011ad39296848566bef85d9dc6242731dcf1
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-03-16 13:06:45 +0100

    Add Matrix ADT concept to data structures
    Added a skeleton concept for MatrixAdt to be used as interface
    for all supported matrices types in the future.

---
+++ include/data-structures/matrix-concept/MatrixAdt.hpp
@@ -0,0 +1,31 @@
+#pragma once
+
+#include <concepts>
+
+/// @brief namespace for data structures implemented
+namespace data_structures {
+
+/// @brief namespace for data structures implementation details
+namespace detail {
+/// @brief a tag to be used to strict matrices types only
+/// @note it can be used in an intrusive approach via inheritance (preferred
+///       privately tho), or non-intrusively through specialization
+class MatrixAdtTag {};
+
+/// @brief this indirection to limit classes to MatrixAdt type
+/// @tparam T the type in question
+template <typename T>
+class IsMatrixAdt : public std::is_base_of<MatrixAdtTag, T> {};
+
+/// @brief a compile-time non-intrusive indirecion that utilizes above class
+///       definition
+/// @tparam T the type of question
+template <typename T>
+constexpr bool IsMatrixAdt_v = IsMatrixAdt<T>::value;
+
+/// @brief concept for Matrix ADT interface
+template <typename T>
+concept MatrixAdt = IsMatrixAdt_v<T>;
+
+}  // namespace detail
+}  // namespace data_structures
