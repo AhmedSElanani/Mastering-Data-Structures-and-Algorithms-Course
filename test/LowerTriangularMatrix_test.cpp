09b419cfb3df852aa039f7dabe36e6e4d693ab9d
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-03-26 22:48:28 +0100

    Add tests for LowerTriangularMatrix mult.
    Added unit tests for LowerTriangularMatrix multiplication operator,
    while marked remaining tests that need to be added as TODO

--- test/LowerTriangularMatrix_test.cpp
+++ test/LowerTriangularMatrix_test.cpp
@@ -73,6 +73,39 @@ TEST(TestingConstruction,
   }
 }
 
+// TODO:
+// test dimensions same as normal matrix
+
+// TODO:
+// test row() method
+
+// TODO:
+// test column() method
+
+TEST(TestingMultiplication, LowerTriangularMatricesMultiplication) {
+  // TODO: complete this test suite
+  {
+    const auto result{
+        LowerTriangularMatrix<3U>{{1U}, {2U, 3U}, {4U, 5U, 6U}} *
+        LowerTriangularMatrix<3U>{{7U}, {8U, 9U}, {10U, 11U, 12U}}};
+
+    EXPECT_STREQ(result.display().c_str(), "|7 0 0|\n|38 27 0|\n|128 111 72|");
+  }
+
+  {
+    const auto result{
+        LowerTriangularMatrix<4U>{
+            {1U}, {2U, 3U}, {4U, 5U, 6U}, {7U, 8U, 9U, 10U}} *
+        LowerTriangularMatrix<4U>{
+            {11U}, {12U, 13U}, {14U, 15U, 16U}, {17U, 18U, 19U, 20U}}
+
+    };
+
+    EXPECT_STREQ(result.display().c_str(),
+                 "|11 0 0 0|\n|58 39 0 0|\n|188 155 96 0|\n|469 419 334 200|");
+  }
+}
+
 TEST(TestingDisplay, DisplayLowerTriangleMatricesOfDifferentSizes) {
   EXPECT_STREQ((LowerTriangularMatrix<1U>{}).display().c_str(), "|0|");
   EXPECT_STREQ((LowerTriangularMatrix<1U>{{1U}}).display().c_str(), "|1|");
