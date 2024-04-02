f0ec0e4bb74b0ec84b8aede9952dd02dac121516
Ahmed Elanani <AhmedSElanani@gmail.com>
2024-04-02 20:03:25 +0200

    Add a workaround for unknown compiler options

--- scripts/run-clang-tidy.sh
+++ scripts/run-clang-tidy.sh
@@ -1,6 +1,11 @@
 #!/bin/bash
 
+# remove unrecognized arguments to clang-tidy from compile_commands.json before execution
+compile_command_path="build/compile_commands.json"
+sed -i 's/-fconcepts-diagnostics-depth=3//g' $compile_command_path
+
+
 # This script runs clang-tidy on .cpp, .hpp, and .h files,
 # in the include and tests directories according to the rules
 # specified at: .clang-tidy
-find include -type f \( -regex ".*\.[ch]?.pp" -o -name "*.h" \) -exec clang-tidy -extra-arg=-Wno-unknown-warning-option --fix-errors -p build/compile_commands.json {} \;
+find include -type f \( -regex ".*\.[ch]?.pp" -o -name "*.h" \) -exec clang-tidy -extra-arg=-Wno-unknown-warning-option --fix-errors -p $compile_command_path {} \;
